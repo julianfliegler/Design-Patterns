bd6881c2340d5380ed064a0b4e8a1d14359ea418
Julian Fliegler <julianfliegler@gmail.com>
2022-02-13 20:02:41 -0800

    Add files via upload

---
+++ Singleton/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Testing Logger");
+    Test t1 = new Test(1);
+    Test t2 = new Test(2);
+    t1.start();
+    t2.start();
+  }
+}
---
+++ Singleton/Test.java
@@ -0,0 +1,19 @@
+public class Test extends Thread{
+  private int id;
+  private int count = 0; // test counts
+
+  public Test(int identifier){
+    id = identifier;
+    System.out.println("Creating new Test Thread with ID: " + id);
+  }
+
+  public void run(){
+    while(count < 10){
+      try{
+        sleep(100);
+      } catch (InterruptedException e){}
+      count++;
+      Logger.getInstance().log("Thread: " + id + " writes " + count + "th msg to log.");
+    }
+  }
+}
